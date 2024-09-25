6b93ae35efdb8893887ae6a89acf8ca29003e67d
Renato Oliveira <renato.dev.oliveira@gmail.com>
2024-09-25 09:56:00 -0300

    fixed const name `databaseMaxConnectionResult` to `databaseMaxxConnectionsResult`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -6,8 +6,8 @@ async function status(request, response) {
   const databaseVersionResult = await database.query("SHOW server_version;");
   const databaseVersionValue = databaseVersionResult.rows[0].server_version;
 
-  const databaseMaxConnectionResult = await database.query("SHOW max_connections;");
-  const databaseMaxConnectionsValue = databaseMaxConnectionResult.rows[0].max_connections;
+  const databaseMaxConnectionsResult = await database.query("SHOW max_connections;");
+  const databaseMaxConnectionsValue = databaseMaxConnectionsResult.rows[0].max_connections;
 
   const databaseName = process.env.POSTGRES_DB;
   const databaseOpenedConnectionsResult = await database.query({
